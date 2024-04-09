44792951722d7f563ecc684d1ebb028866a649dd
anhvuknguyen <avnguyen1115@gmail.com>
2024-04-08 23:17:16 -0500

    dictionary

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ file_path = 'pathologyOfTheKidney.pdf'
 images_path = 'images/'
 pdf_file = fitz.open(file_path)
 page_nums = len(pdf_file)
+num = 1
+dictionary = {} 
 
 c = canvas.Canvas("PdfWithImages.pdf", pagesize=A4)
 width, height = A4
@@ -17,15 +19,18 @@ width, height = A4
 border = 20 # pixels that there is border around each image so it doesn't fill the entire paper
 width -= (border * 2)
 height -= (border * 2)
+
 for i in range(page_nums):
     for item in pdf_file.get_page_images(i):
-        pix = fitz.Pixmap(pdf_file, item[0])  # pixmap from the image xref
+        pix = fitz.Pixmap(pdf_file, item[0]) # pixmap from the image xref
         pix0 = fitz.Pixmap(fitz.csRGB, pix)  # force into RGB
-        pix0.save("img%i.jpg" % item[0])
+        pix0.save("img%i.jpg" % num)
+        dictionary["img%i" % num]="img%i.jpg" % num
+        
 
 
         # put all images in a pdf
-        img = Image.open("img%i.jpg" % item[0])  # open image
+        img = Image.open("img%i.jpg" % num)  # open image
         img_width, img_height = img.size  # save image width and size
 
         # rescale the image so it fits the page even with the borders
@@ -39,19 +44,24 @@ for i in range(page_nums):
 
         # resize the image, antialias (resampling.lanczos) to make the picture not blurry/weird
         img = img.resize((int(new_width), int(new_height)), Resampling.LANCZOS)
-        img.save("img%i.jpg" % item[0])
+        img.save("img%i.jpg" % num)
 
         # calculate image position so its centered w/ borders
         img_x = border + (width - new_width) / 2
         img_y = border + (height - new_height) / 2
 
         # put image on pdf
-        c.drawImage("img%i.jpg" % item[0],  img_x, img_y)
+        c.drawImage("img%i.jpg" % num,  img_x, img_y)
         c.showPage()
+
+        num+=1
 
 
 c.save()
 
+print(dictionary['img1'])
+im = Image.open(dictionary['img1'],'r')
+im.show()
 
 
 
